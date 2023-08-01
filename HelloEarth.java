93053f50dc339695880349facc60e4023e71eb96
Ashish Pandey <113827973+AshishPandey26@users.noreply.github.com>
2023-08-01 06:55:41 +0530

    Adding Files
    Currently running these via CMD to understand the whole undergoing process and structure of JAVA

---
+++ HelloEarth.java
@@ -0,0 +1,5 @@
+class HelloEarth{
+	public static void main(String[] args){
+		System.out.println("hello");
+	}
+}
---
+++ Playlist/First.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class First {
+
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int age = sc.nextInt();
+            if (age%2==0){
+                System.out.println("even");
+            }else{
+                System.out.println("odd");
+            }
+        }
+    }
