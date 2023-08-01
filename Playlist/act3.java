1b054b111e499a86d27a125796a28e8273fcecc8
AshishPandey26 <pandaya10@gmail.com>
2023-08-01 22:15:12 +0530

    adding new

---
+++ Playlist/act3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class act3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int inp = sc.nextInt();
+
+        System.out.println(inp);
+
+        if (inp==1)
+            System.out.println("Hello");
+        else if(inp == 2)
+            System.out.println("NAMASTE");
+        else
+            System.out.println("Bonjour");
+
+    }
+}
